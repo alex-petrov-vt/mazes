cecfbf99827e384bdb85124513590bcdb1126737
Alex Petrov <alex.petrov.vt@gmail.com>
2021-11-26 13:45:24 -0500

    Store links to linked cells in a cell struct

--- cell.go
+++ cell.go
@@ -3,8 +3,20 @@ package main
 type cell struct {
 	row, col                 int
 	north, east, south, west *cell
+	links                    map[*cell]bool
 }
 
 func newCell(row, col int) *cell {
-	return &cell{row: row, col: col}
+	links := make(map[*cell]bool)
+	return &cell{row: row, col: col, links: links}
+}
+
+func (c *cell) link(other *cell) {
+	c.links[other] = true
+	other.links[c] = true
+}
+
+func (c *cell) unlink(other *cell) {
+	delete(c.links, other)
+	delete(other.links, c)
 }
--- cell_test.go
+++ cell_test.go
@@ -15,4 +15,21 @@ func TestNewCell(t *testing.T) {
 	assert.Nil(t, s.east)
 	assert.Nil(t, s.south)
 	assert.Nil(t, s.west)
+	assert.Empty(t, s.links)
+}
+
+func TestLinkingCells(t *testing.T) {
+	c1 := newCell(1, 1)
+	c2 := newCell(1, 2)
+
+	c1.link(c2)
+	// Make sure linking is bidirectional
+	assert.True(t, c1.links[c2])
+	assert.True(t, c2.links[c1])
+
+	c1.unlink(c2)
+	_, check := c1.links[c2]
+	assert.False(t, check)
+	_, check = c2.links[c1]
+	assert.False(t, check)
 }
